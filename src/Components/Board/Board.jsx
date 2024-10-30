b6a5cbab37e3f16b43951332d8be9aec34df85cb
aadhi-11 <adithyadasaadhiii@gmail.com>
2024-10-30 18:42:06 +0530

    ArraySlice redux

--- src/Components/Board/Board.jsx
+++ src/Components/Board/Board.jsx
@@ -1,24 +1,29 @@
-import React from 'react'
+import React from 'react';
 import Square from '../square/Square';
-import './Board.css'
+import './Board.css';
 
-
-
-const BoardFill = (num) => {
+const BoardFill = ({ num }) => {  
     const TotalComponents = [];
     for (let i = 0; i < num; i++) {
         TotalComponents.push(<Square key={`${i}`} />);
     }
-    return <div className='row' >{TotalComponents}</div>
-
-}
-
-const Board = () => {
-    
-
-    return <div className="board">
-        {BoardFill(5)}
-    </div>;
+    return <div className='row'>{TotalComponents}</div>;
 };
 
-export default Board
+const mappedBoard = (num) => {
+    const component = [];
+    for (let i = 0; i < num; i++) {
+        component.push(<BoardFill key={`row-${i}`} num={num} />);  
+    }
+    return <div className="Board">{component}</div>;
+};
+
+const Board = () => {
+    return (
+        <>
+            {mappedBoard(4)}
+        </>
+    );
+};
+
+export default Board;
--- src/Components/square/Square.jsx
+++ src/Components/square/Square.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './Squares.css'
 
 const Square = () => {
+  const [value,setValue]=useState(null)
+  const randomGenerator = () =>{
+    const val = Math.floor(Math.random() * 11);
+    if(value===null)     setValue(val);
+  }
   return (
-    <div className='square' >
-        10
+    <div className='square'  onClick={randomGenerator} >
+        <span style={{color:'white'}} >{value}</span> 
     </div>
   )
 }
